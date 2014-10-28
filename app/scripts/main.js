6a4aef5185d3d57be65fe69b6f9d90cf5e862938
Andrew El-Masry <andrewelmasry@gmail.com>
2014-10-28 10:36:47 -0400

    working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,19 @@ var library = {
   ]
 };
 
+var someApi = {
+  dvds : [
+    { name: 'Fun with Dick and Jane', company: 'New Line Cinemas' },
+    { name: 'Game of Thrones', company: 'HBO'},
+    { name: 'Back to the Future', company: 'Universal Studios'}
+  ]
+};
+
 //Creating our handlebars function
 var rendered= Handlebars.templates['books'];
 
 $('#books').append( rendered(library) );
+
+var rendered2= Handlebars.templates['dvds'];
+
+$('#dvds').append( rendered2(someApi) );
--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -13,4 +13,17 @@ templates['books'] = template({"1":function(depth0,helpers,partials,data) {
   if (stack1 != null) { buffer += stack1; }
   return buffer;
 },"useData":true});
+templates['dvds'] = template({"1":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "    <li> "
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + " by "
+    + escapeExpression(((helper = (helper = helpers.company || (depth0 != null ? depth0.company : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"company","hash":{},"data":data}) : helper)))
+    + " </li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var stack1, buffer = "";
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.dvds : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { buffer += stack1; }
+  return buffer;
+},"useData":true});
 })();
